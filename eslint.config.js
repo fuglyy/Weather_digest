87c0be8c8f2b70737ebdb326aede99e260d7aa56
fuglyy <sp.yanusya@gmail.com>
2026-09-13 18:06:06 +0300

    docs: add project documentation and configuration

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        fetch: 'readonly',
+        AbortController: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        URL: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
